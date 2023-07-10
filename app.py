bb945e499cc406d0b302c6c8d79ea4f15e46e63c
Sandip Gangakhedkar <sandip@imperiumdrive.com>
2023-07-10 14:58:31 +0100

    feat: Adding Wikipedia API Wrapper

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, SequentialChain
 from langchain.memory import ConversationBufferMemory
-
+from langchain.utilities import WikipediaAPIWrapper
 
 os.environ["OPENAI_API_KEY"] = apikey
 
@@ -16,8 +16,9 @@ title_template = PromptTemplate(
 )
 
 script_template = PromptTemplate(
-    input_variables=["title"],
-    template="Write me a youtube video script based on this title TITLE: {title}.",
+    input_variables=["title", "wikipedia_research"],
+    template="Write me a youtube video script based on this title TITLE: {title} \
+        while leveraging this wikipedia research: {wikipedia_research}",
 )
 
 # App framework
@@ -25,28 +26,41 @@ st.title("🦜️🔗 YouTube GPT Creator")
 prompt = st.text_input("Enter a prompt for the AI to complete")
 
 # Memory
-memory = ConversationBufferMemory(input_key="topic", memory_key="chat_memory")
+title_memory = ConversationBufferMemory(input_key="topic", memory_key="chat_history")
+script_memory = ConversationBufferMemory(input_key="title", memory_key="chat_history")
 
 # LLMs
 llm = OpenAI(temperature=0.9)
 title_chain = LLMChain(
-    llm=llm, prompt=title_template, verbose=True, output_key="title", memory=memory
+    llm=llm,
+    prompt=title_template,
+    verbose=True,
+    output_key="title",
+    memory=title_memory,
 )
 script_chain = LLMChain(
-    llm=llm, prompt=script_template, verbose=True, output_key="script", memory=memory
-)
-sequential_chain = SequentialChain(
-    chains=[title_chain, script_chain],
-    input_variables=["topic"],
-    output_variables=["title", "script"],
+    llm=llm,
+    prompt=script_template,
     verbose=True,
+    output_key="script",
+    memory=script_memory,
 )
+
+# Wikipedia API wrapper
+wiki = WikipediaAPIWrapper()
 
 # Show stuff to the screen if there's a prompt
 if prompt:
-    response = sequential_chain({"topic": prompt})
-    st.write(response["title"])
-    st.write(response["script"])
+    title = title_chain.run(topic=prompt)
+    wiki_research = wiki.run(prompt)
+    script = script_chain.run(title=title, wikipedia_research=wiki_research)
 
-    with st.expander("Message History"):
-        st.info(memory.buffer)
+    st.write(title)
+    st.write(script)
+
+    with st.expander("Title History"):
+        st.info(title_memory.buffer)
+    with st.expander("Script History"):
+        st.info(script_memory.buffer)
+    with st.expander("Wikipedia Research"):
+        st.info(wiki_research)
