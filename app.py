db3afcaa979241b927ff3a3d77e17bcca8f541f2
Sandip Gangakhedkar <sandip@imperiumdrive.com>
2023-07-10 14:27:54 +0100

    feat: Adding a SimpleSequentialChain

--- app.py
+++ app.py
@@ -4,13 +4,18 @@ from apikey import apikey
 import streamlit as st
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
+from langchain.chains import LLMChain, SimpleSequentialChain
 
 os.environ["OPENAI_API_KEY"] = apikey
 
-# Prompt template
+# Prompt templates
 title_template = PromptTemplate(
     input_variables=["topic"], template="Write me a youtube video title about {topic}."
+)
+
+script_template = PromptTemplate(
+    input_variables=["title"],
+    template="Write me a youtube video script based on this title TITLE: {title}.",
 )
 
 # App framework
@@ -20,8 +25,12 @@ prompt = st.text_input("Enter a prompt for the AI to complete")
 # LLMs
 llm = OpenAI(temperature=0.9)
 title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True)
+script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True)
+sequential_chain = SimpleSequentialChain(
+    chains=[title_chain, script_chain], verbose=True
+)
 
 # Show stuff to the screen if there's a prompt
 if prompt:
-    response = title_chain.run(prompt)
+    response = sequential_chain.run(prompt)
     st.write(response)
